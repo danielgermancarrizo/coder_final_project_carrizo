797b8344276fe99b357ebf86cfab0e1e4e5cee3c
Daniel Carrizo <daniel.german.carrizo@gmail.com>
2022-04-17 10:07:40 -0300

    first commit

---
+++ final_project_carrizo/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from final_project_carrizo.users.models import User
+from final_project_carrizo.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
